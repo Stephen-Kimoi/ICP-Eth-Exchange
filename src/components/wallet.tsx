cdfd1e8168a67abf5b9a44fe7ff8d5be2541a0be
Stephen-Kimoi <moenisteve@gmail.com>
2024-01-23 08:30:36 +0300

    creating a new wallet component

---
+++ src/components/wallet.tsx
@@ -0,0 +1,25 @@
+import { useAccount, useConnect, useDisconnect } from "wagmi"
+import { injected } from "wagmi/connectors" 
+
+interface WalletProps {} 
+
+const Wallet: React.FC<WalletProps> = ({}) => {
+   const { address } = useAccount() 
+
+   const { connect } = useConnect()
+ 
+   const { disconnect } = useDisconnect(); 
+
+   if (address)
+    return (
+      <main>
+        Connected to: {address} 
+        <br/> 
+        <button onClick={ () => disconnect() }>Disconnect</button>
+      </main>
+    )
+   
+    return <button onClick={ () =>   connect({ connector: injected() })  }>Connect Wallet </button>
+
+}
+
